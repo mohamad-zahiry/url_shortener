cd9a4360073874ce2fff89194a513e588fe4c3f3
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-03-01 22:29:28 +0330

    Add "login" path

--- Ushort/urls.py
+++ Ushort/urls.py
@@ -6,5 +6,6 @@ app_name = "Ushort"
 urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.sign_up, name="signup"),
+    path("login/", views.log_in, name="login"),
     path("logout/", views.log_out, name="logout"),
 ]
