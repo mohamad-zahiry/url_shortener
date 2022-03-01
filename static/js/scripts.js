f600d69ec6aa97c55ea587325f867721b023e7b0
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-03-01 15:50:00 +0330

    Fix a bug about signup page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -97,53 +97,6 @@
 		}
     });
 
-
-    /* Video Lightbox - Magnific Popup */
-    $('.popup-youtube, .popup-vimeo').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false,
-        iframe: {
-            patterns: {
-                youtube: {
-                    index: 'youtube.com/', 
-                    id: function(url) {        
-                        var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
-                        if ( !m || !m[1] ) return null;
-                        return m[1];
-                    },
-                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
-                },
-                vimeo: {
-                    index: 'vimeo.com/', 
-                    id: function(url) {        
-                        var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
-                        if ( !m || !m[5] ) return null;
-                        return m[5];
-                    },
-                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
-                }
-            }
-        }
-    });
-
-
-    /* Details Lightbox - Magnific Popup */
-	$('.popup-with-move-anim').magnificPopup({
-		type: 'inline',
-		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
-		fixedBgPos: true,
-		overflowY: 'auto',
-		closeBtnInside: true,
-		preloader: false,
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
-    
     
     /* Move Form Fields Label When User Types */
     // for input and textarea fields
@@ -162,34 +115,9 @@
             // handle the invalid form...
             sformError();
             ssubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            ssubmitForm();
-        }
+        } 
     });
 
-    function ssubmitForm() {
-        // initiate variables with form content
-		var email = $("#semail").val();
-		var name = $("#sname").val();
-		var password = $("#spassword").val();
-        var terms = $("#sterms").val();
-        
-        $.ajax({
-            type: "POST",
-            url: "php/signupform-process.php",
-            data: "email=" + email + "&name=" + name + "&password=" + password + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    sformSuccess();
-                } else {
-                    sformError();
-                    ssubmitMSG(false, text);
-                }
-            }
-        });
-	}
 
     function sformSuccess() {
         $("#signUpForm")[0].reset();
