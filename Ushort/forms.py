fbca9f7d08278515623482a2047367d28dc768e1
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-03-01 22:27:48 +0330

    Add "CreatorLogInForm"

--- Ushort/forms.py
+++ Ushort/forms.py
@@ -19,3 +19,15 @@ class CreatorSignUpForm(Form):
         email, password = self.cleaned_data.values()
         user = User.objects.create_user(username=email, password=password)
         Creator.objects.create(user=user, email=email)
+
+
+class CreatorLogInForm(Form):
+    email = EmailField(widget=EmailInput(attrs={"class": "form-control-input", "id": "lemail"}))
+    password = CharField(widget=PasswordInput(attrs={"class": "form-control-input", "id": "lpassword"}))
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email").lower()
+        creator = Creator.objects.filter(email=email)
+        if not creator.exists():
+            raise ValidationError(_("This email has not been registered before"))
+        return email
