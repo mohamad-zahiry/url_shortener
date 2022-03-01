0a42c0ced82537aa8a262fbda2a1da01462eb2af
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-03-01 15:48:10 +0330

    Create "signup" view

--- Ushort/views.py
+++ Ushort/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
+from Ushort.forms import CreatorSignUpForm
+from Ushort.models import Creator
 
 
 def home(request):
     return render(request, "Ushort/home.html", {})
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("Ushort:home")  # ! fix: redirect to "user panel"
+
+    form = CreatorSignUpForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+        user = Creator.objects.get(email=form.cleaned_data.get("email")).user
+        login(request, user)
+
+        return redirect("Ushort:home")  # ! fix: redirect to "user panel"
+
+    context = {"form": form}
+    return render(request, "Ushort/signup.html", context)
