b51b5e76cc0b8fc95b4a12c4147519b0f13b807c
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-04-11 22:17:01 +0430

    Add "url_list_view"

--- Ushort/api/views.py
+++ Ushort/api/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from Ushort.api.serializers import UrlCreateSerializer
-from Ushort.models import Creator
+from Ushort.api.serializers import UrlCreateSerializer, UrlListSerializer
+from Ushort.models import Creator, Url
 
 
 @login_required(login_url="Ushort:login")
@@ -19,3 +19,15 @@ def url_add(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@login_required(login_url="Ushort:login")
+@api_view(["POST"])
+def url_list_view(request):
+    creator = Creator.by_request(request)
+    if creator:
+        urls = Url.objects.filter(creator=creator)
+        serializer = UrlListSerializer(instance=urls, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(data="Unautherized", status=status.HTTP_401_UNAUTHORIZED)
