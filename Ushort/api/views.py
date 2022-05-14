5ff2cc9e5c407df57d67b6ad7b6e569c9fe61783
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-05-15 00:09:23 +0430

    Add authentication to "url_add"

--- Ushort/api/views.py
+++ Ushort/api/views.py
@@ -1,7 +1,10 @@
+from cgi import print_arguments
 from django.contrib.auth.decorators import login_required
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from Ushort.api.serializers import UrlCreateSerializer, UrlListSerializer
 from Ushort.models import Creator, Url
@@ -9,6 +12,8 @@ from Ushort.models import Creator, Url
 
 @login_required(login_url="Ushort:login")
 @api_view(["POST"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def url_add(request):
     serializer = UrlCreateSerializer(data=request.data)
     creator = Creator.by_request(request)
