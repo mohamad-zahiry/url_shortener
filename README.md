7645b574870dc3e0e4383db6af4c82df38c8938c
rattin <zahiri.80m@gmail.com>
2022-08-04 20:29:58 +0430

    Restarting the project

--- README.md
+++
@@ -1,9 +0,0 @@
-# url_shortener
-
-A feature full url shortener powered up by django
-
-## Features
-- two url types
-    - simple
-    - monitored
-- save visitors numbers, country and hour
--- Ushort/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from Ushort.api import views
-
-
-urlpatterns = [
-    path("url/add/", views.url_add, name="api_url-add"),
-    path("url/list/", views.url_list_view, name="api_url-list"),
-]
