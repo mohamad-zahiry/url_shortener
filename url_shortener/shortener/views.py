37f9f0e9d6bca204622a92918c5341991c2e174a
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-05 20:40:33 +0430

    Create veiw for creating short url

--- url_shortener/shortener/views.py
+++ url_shortener/shortener/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from shortener.models import ShortenedUrl
+
+
+class ShortenedUrlCreateView(CreateView):
+    model = ShortenedUrl
+    fields = ("url", "active_from", "active_until")
+    template_name = "shortener/shortened_url_create_view.html"
+
+    def get_success_url(self):
+        return reverse("shortener:create")
