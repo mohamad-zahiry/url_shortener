05c9d6a0c76770c8bbe1817ed4eaba3cde054d66
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-05 19:28:58 +0430

    Create ShortenedUrl model

--- url_shortener/shortener/models.py
+++ url_shortener/shortener/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ShortenedUrl(models.Model):
+    url = models.URLField(max_length=300)
+    key = models.CharField(max_length=15, unique=True, blank=False)
+
+    active_from = models.DateTimeField()
+    active_until = models.DateTimeField()
+
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.key
