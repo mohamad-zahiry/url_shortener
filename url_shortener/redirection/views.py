486991fa07d1cf970cdfe490bc732017e2710b44
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-06 19:30:07 +0430

    Add url redirection view

--- url_shortener/redirection/views.py
+++ url_shortener/redirection/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.base import RedirectView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from shortener.models import ShortenedUrl
+
+
+class UrlRedirectView(RedirectView):
+    query_string = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        url = get_object_or_404(ShortenedUrl, key=kwargs["key"]).url
+        return url
