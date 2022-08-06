5128c3b239253dd1c774ec155324c02ad8944786
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-06 19:31:47 +0430

    Include redirection.urls to main urls

--- url_shortener/url_shortener/urls.py
+++ url_shortener/url_shortener/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("shortener/", include("shortener.urls")),
+    path("redirect/", include("redirection.urls")),
 ]
