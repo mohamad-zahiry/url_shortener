2d55ee863b47d76eb78cc996e455028eba6620ce
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-06 22:29:55 +0430

    Add route for url detail view

--- url_shortener/shortener/urls.py
+++ url_shortener/shortener/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from shortener.views import ShortenedUrlCreateView
+from shortener.views import ShortenedUrlCreateView, ShortenedUrlDetailView
 
 
 app_name = "shortener"
 
 urlpatterns = [
     path("create/", view=ShortenedUrlCreateView.as_view(), name="create"),
+    path("detail/<str:key>/", view=ShortenedUrlDetailView.as_view(), name="url_detail"),
 ]
