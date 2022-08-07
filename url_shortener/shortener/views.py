14802cde76895c5d0d9b4a444f30b99032667b79
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-07 12:47:16 +0430

    Add update url view

--- url_shortener/shortener/views.py
+++ url_shortener/shortener/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 
 from shortener.models import ShortenedUrl
 
@@ -17,5 +18,13 @@ class ShortenedUrlCreateView(CreateView):
 class ShortenedUrlDetailView(DetailView):
     model = ShortenedUrl
     template_name = "shortener/shortened_url_detail_view.html"
+    slug_field = "key"
+    slug_url_kwarg = "key"
+
+
+class ShortenedUrlUpdateView(UpdateView):
+    model = ShortenedUrl
+    template_name = "shortener/shortened_url_update_view.html"
+    fields = ("active_from", "active_until")
     slug_field = "key"
     slug_url_kwarg = "key"
