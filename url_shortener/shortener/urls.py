b46fea63ec385ee97ebb08380d9c67938613d37c
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-07 12:47:29 +0430

    Add route for update url

--- url_shortener/shortener/urls.py
+++ url_shortener/shortener/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from shortener.views import ShortenedUrlCreateView, ShortenedUrlDetailView
+from shortener.views import (
+    ShortenedUrlCreateView,
+    ShortenedUrlDetailView,
+    ShortenedUrlUpdateView,
+)
 
 
 app_name = "shortener"
@@ -7,4 +11,5 @@ app_name = "shortener"
 urlpatterns = [
     path("create/", view=ShortenedUrlCreateView.as_view(), name="create"),
     path("detail/<str:key>/", view=ShortenedUrlDetailView.as_view(), name="url_detail"),
+    path("update/<str:key>/", view=ShortenedUrlUpdateView.as_view(), name="url_update"),
 ]
