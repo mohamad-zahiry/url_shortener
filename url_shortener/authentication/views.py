08d94a96cd7c761874fcb8898c6024307055451b
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-08 12:41:13 +0430

    Add login view and template

--- url_shortener/authentication/views.py
+++ url_shortener/authentication/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = "authentication/login.html"
+    redirect_authenticated_user = True
+    next_page = reverse_lazy("shortener:create")
