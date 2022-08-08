56261b8fda8b2054b2d196f1f2b61ca981994c7a
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-08 14:58:04 +0430

    Add logout view

--- url_shortener/authentication/views.py
+++ url_shortener/authentication/views.py
@@ -1,8 +1,12 @@
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class CustomLoginView(LoginView):
     template_name = "authentication/login.html"
     redirect_authenticated_user = True
     next_page = reverse_lazy("shortener:create")
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy("authentication:login")
