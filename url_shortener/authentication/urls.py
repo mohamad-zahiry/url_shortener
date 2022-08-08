680b32195106ab3afa51f28007b11382274d08b4
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-08 14:58:19 +0430

    Add route for logout

--- url_shortener/authentication/urls.py
+++ url_shortener/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from authentication.views import CustomLoginView
+from authentication.views import CustomLoginView, CustomLogoutView
 
 app_name = "authentication"
 
 urlpatterns = [
     path("login/", view=CustomLoginView.as_view(), name="login"),
+    path("logout/", view=CustomLogoutView.as_view(), name="logout"),
 ]
