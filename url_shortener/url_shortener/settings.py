f3d6fed1b529a1f7449b3708d8ec8ac279ff8ad0
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-09 20:31:33 +0430

    Change database to Postgres

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "url_shortener.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_db",
+        "USER": "test_user",
+        "PASSWORD": "1234",
+        "HOST": "172.17.0.2",
+        "PORT": "5432",
     }
 }
 
