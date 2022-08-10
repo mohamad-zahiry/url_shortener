6d8c5277a6c0fb0fa45199d719e98f527e50771d
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-10 14:49:19 +0430

    Update each urldata at redirections
    In any redirection, a record which contains url key, visitor country,
    date and hour, updated or created.

--- url_shortener/redirection/views.py
+++ url_shortener/redirection/views.py
@@ -1,12 +1,17 @@
 from django.views.generic.base import RedirectView
 from django.shortcuts import get_object_or_404
+from ipware import get_client_ip
 
 from shortener.models import ShortenedUrl
+from redirection.utils.url_data import increase_visits_or_create
+from redirection.models import Country
 
 
 class UrlRedirectView(RedirectView):
     query_string = False
 
     def get_redirect_url(self, *args, **kwargs):
-        url = get_object_or_404(ShortenedUrl, key=kwargs["key"]).url
-        return url
+        country = Country.from_ip(get_client_ip(self.request)[0])
+        obj = get_object_or_404(ShortenedUrl, key=kwargs["key"])
+        increase_visits_or_create(key=obj, country=country)
+        return obj.url
