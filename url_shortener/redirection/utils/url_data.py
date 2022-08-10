0b75fbe1d84ea367c42db3690a34b56ae29585c6
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-10 14:46:12 +0430

    Add util func to increase UrlData visits
    If a UrlData don't exits, it make a new record

---
+++ url_shortener/redirection/utils/url_data.py
@@ -0,0 +1,17 @@
+from django.utils import timezone
+
+from redirection.models import UrlData, now_timeframe, Country
+
+
+def increase_visits_or_create(
+    key,
+    country=Country.objects.get_or_create(name="")[0],
+):
+    obj, _ = UrlData.objects.get_or_create(
+        key=key,
+        country=country,
+        hour=now_timeframe(),
+        date=timezone.now(),
+    )
+    obj.visits += 1
+    obj.save()
