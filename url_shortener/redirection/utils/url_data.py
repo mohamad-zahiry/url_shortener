093891bf309bfcc5b22cc7a5f4034ed5c479b2d7
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-11 18:43:14 +0430

    Better syntax. Use None as default argument

--- url_shortener/redirection/utils/url_data.py
+++ url_shortener/redirection/utils/url_data.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 from redirection.models import UrlData, now_timeframe, Country
 
 
-def increase_visits_or_create(
-    key,
-    country=Country.objects.get_or_create(name="")[0],
-):
+def increase_visits_or_create(key, country=None):
+    if not country:
+        country = Country.objects.get_or_create(name="")[0]
+
     obj, _ = UrlData.objects.get_or_create(
         key=key,
         country=country,
