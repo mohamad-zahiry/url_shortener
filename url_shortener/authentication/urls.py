fe457759b80355c1fcaa8048aba559a5a840d1be
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-13 18:27:25 +0430

    Add route for signup page

--- url_shortener/authentication/urls.py
+++ url_shortener/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from authentication.views import CustomLoginView, CustomLogoutView
+from authentication.views import CustomLoginView, CustomLogoutView, SignupView
 
 app_name = "authentication"
 
 urlpatterns = [
     path("login/", view=CustomLoginView.as_view(), name="login"),
     path("logout/", view=CustomLogoutView.as_view(), name="logout"),
+    path("signup/", view=SignupView.as_view(), name="signup"),
 ]
