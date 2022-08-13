506286ba3ef79d346eac7c4ad4db1f2a39bae6cb
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-08-13 21:43:16 +0430

    Use request.user for profile view
    Change profile route: remove <str:username>

--- url_shortener/authentication/urls.py
+++ url_shortener/authentication/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path("login/", view=CustomLoginView.as_view(), name="login"),
     path("logout/", view=CustomLogoutView.as_view(), name="logout"),
     path("signup/", view=SignupView.as_view(), name="signup"),
-    path("profile/<str:username>/", view=ProfileView.as_view(), name="profile"),
+    path("profile/", view=ProfileView.as_view(), name="profile"),
 ]
--- url_shortener/authentication/views.py
+++ url_shortener/authentication/views.py
@@ -1,19 +1,13 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
-from requests import request
-
-from redirection.models import UrlData
 
 
 class ProfileView(LoginRequiredMixin, DetailView):
-    model = User
-    slug_field = "username"
-    slug_url_kwarg = "username"
     template_name = "authentication/profile.html"
 
     def get_context_data(self, **kwargs):
@@ -22,11 +16,16 @@ class ProfileView(LoginRequiredMixin, DetailView):
         context["urls"] = urls
         return context
 
+    def get_object(self, queryset=None):
+        user = self.request.user
+        if user.is_authenticated:
+            return user
+        return redirect("authentication:login")
+
 
 class CustomLoginView(LoginView):
     template_name = "authentication/login.html"
     redirect_authenticated_user = True
-    next_page = reverse_lazy("shortener:create")
 
 
 class CustomLogoutView(LogoutView):
